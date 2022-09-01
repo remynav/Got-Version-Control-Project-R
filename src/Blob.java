1d8a663ca92bb0fd6cdcc621b2aebffc6449c1a0
iwiener1 <isaacewiener@icloud.com>
2022-09-01 10:45:50 -0700

    Added untested filewriting

--- src/Blob.java
+++ src/Blob.java
@@ -6,14 +6,19 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Blob {
+	private String sha1;
 	
-	public Blob(String fileName) {
+	public Blob(String fileName) throws IOException {
 		Path filePath = Path.of(fileName);
-		try {
-			String fileText = Files.readString(filePath);
-			
-			String sha1 = generateSha1(fileText);
-		} catch (IOException e) {}
+		String fileText = Files.readString(filePath);
+		
+		this.sha1 = generateSha1(fileText);
+	
+		//writing the file content to the sha1 file
+		writeFile(sha1);
+		FileWriter output = new FileWriter(System.getProperty("user.dir")+File.separator+"objects"+File.separator+sha1);
+		output.write(fileText);
+		output.close();
 	}
 	
 	private static String generateSha1(String text) {
@@ -33,5 +38,20 @@ public class Blob {
 	
 	public static void main (String [] args) {
 		System.out.println(generateSha1("some content"));
+	}
+	
+	private static void writeFile(String fileName) {
+		try {
+		      File file = new File(fileName);
+		      if (file.createNewFile()) {
+		        System.out.println("File created: " + file.getName());
+		      } else {
+		        System.out.println("File already exists.");
+		      }
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		  }
 	}
 }
