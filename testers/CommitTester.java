c2d4ceb07ad64ba5e2b1249d99e31b025deeea5f
Remy navarre <remynav16@gmail.com>
2022-10-03 20:44:09 -0700

    tester stuff

--- testers/CommitTester.java
+++ testers/CommitTester.java
@@ -1,9 +1,13 @@
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 public class CommitTester {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
 		Index i=new Index();
 		i.init();
 		i.add("TestFile1.txt");
-		Commit parentCommit = new Commit();
+		
+		Commit parentCommit = new Commit("first com", "remy", null);
 		
 		
 	}
